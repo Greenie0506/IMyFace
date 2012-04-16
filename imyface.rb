36967b991020cae99995778577557eda052a3965
Greenie0506 <greenie0506@gmail.com>
2012-04-16 19:57:36 -0400

    start of outta_my_face and hash indexing

--- imyface.rb
+++ imyface.rb
@@ -10,6 +10,8 @@ class UserDatabase
   end
 end
 
+@enrolled_users = UserDatabase.new()
+
 class User
   attr_accessor :first_name, :last_name, :password, :posts, :user_id
 
@@ -22,14 +24,13 @@ class User
   end
 
   def publish_post(args)
-    new_post = Post.new(title: args[:title],
-                        date: args[:date],
-                        content: args[:content]
-                       )
+    new_post = Post.new(title: args[:title], date: args[:date], content: args[:content])
     @posts << new_post
   end
 
   def save(database)
+    @enrolled_users = database
+
     already_exists = database.rows.any? do |user|
       user.user_id == self.user_id
     end
@@ -38,6 +39,14 @@ class User
       puts "Sorry, that User ID has already been taken"
     else
       database.add(self)
+    end
+  end
+
+  def outta_my_face(user_id)
+    @enrolled_users.rows.each do |u|
+      if user_id == u.user_id
+        u.posts << self.posts
+      end
     end
   end
 end
@@ -52,5 +61,13 @@ class Post
   end
 end
 
+user_1 = User.new(first_name: "Nick", last_name: "Greenfield", user_id: "Greenie0506", password: "12345")
+user_2 = User.new(first_name: "Bob", last_name: "Jones", user_id: "Jonessss", password: "12345")
 
-@enrolled_users = UserDatabase.new()
+user_1.save(@enrolled_users)
+user_2.save(@enrolled_users)
+
+user_2.publish_post(title: "First post", date: "Today", content: "HERE")
+
+user_2.outta_my_face("Greenie0506")
+
--- user_interface.rb
+++ user_interface.rb
@@ -1,14 +1,22 @@
 load 'imyface.rb'
 
-puts "Welcome to IMyFace User registration!"
-puts "What is your first name?"
-first_name = gets
-puts "Ok, now what is your last name?"
-last_name = gets
-puts "Sweet! Now what do you want your User id to be?"
-user_id = gets
-puts "Awesome. Lastly, what is your top secret password?"
-password = gets
+# puts "Welcome to IMyFace User registration!"
+# puts "What is your first name?"
+# first_name = gets
+# puts "Ok, now what is your last name?"
+# last_name = gets
+# puts "Sweet! Now what do you want your User id to be?"
+# user_id = gets
+# puts "Awesome. Lastly, what is your top secret password?"
+# password = gets
 
-new_user = User.new(first_name: first_name.chomp, last_name: last_name.chomp, user_id: user_id.chomp, password: password.chomp)
-new_user.save(@enrolled_users)
+# user_1 = User.new(first_name: first_name.chomp, last_name: last_name.chomp, user_id: user_id.chomp, password: password.chomp)
+@enrolled_users = UserDatabase.new()
+user_1 = User.new(first_name: "Nick", last_name: "Greenfield", user_id: "Greenie0506", password: "12345")
+user_2 = User.new(first_name: "Bob", last_name: "Jones", user_id: "Jonessss", password: "12345")
+user_1.save(@enrolled_users)
+user_2.save(@enrolled_users)
+
+puts @enrolled_users.rows.inspect
+user_2.outta_my_face("Greenie0506")
+
